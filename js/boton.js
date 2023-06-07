a2018274e5bab09b1fc7f71ed0cdf84e49c0994e
Svargasowz <sergio.vargasramirez.es@gmail.com>
2023-06-07 16:42:23 -0500

    a

--- js/boton.js
+++ js/boton.js
@@ -4,11 +4,17 @@ let choiceBox = document.getElementById("choiceBox");
 /*2 verificamos que clase se encuentra en choice-box*/
 toggleBox.addEventListener("click",function(){
     if(choiceBox.classList.contains("choice-box-off")){
-        toggleBox.classList.add("choice-box-on");
-        toggleBox.classList.remove("choice-box-off")
+        choiceBox.classList.add("choice-box-on");
+        choiceBox.classList.remove("choice-box-off");
+        toggleBox.classList.add("toggle-box-on");
+        toggleBox.classList.add("toggle-box-off");
+
     }
     else{
-        choiceBox.classList.remove("choice-box-on")
+        choiceBox.classList.remove("choice-box-on");
+        choiceBox.classList.add("choice-box-off");
+        toggleBox.classList.remove("toggle-box-on");
+        toggleBox.classList.add("toggle-box-off");
     }
 })
 /*3 al dar click quitamos la clase por defecto de choice-box
