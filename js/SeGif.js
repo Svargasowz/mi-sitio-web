754e8711b14ba64f6fdfad4d78fbbfd5c036a75c
Svargasowz <sergio.vargasramirez.es@gmail.com>
2023-08-22 19:53:55 -0500

    a

--- js/SeGif.js
+++ js/SeGif.js
@@ -1,14 +1,5 @@
-let barMenu = document.getElementById("barMenu");
-let main_Menu = document.getElementById("mainMenu"); 
-
-barMenu.addEventListener("click", function() {
-    if (main_Menu.classList.contains("main-menu")){
-        main_Menu.classList.add("main-menu-visible");
-        main_Menu.classList.remove("main-menu");
-    }
-        else{
-            main_Menu.classList.remove("main-menu-visible");
-            main_Menu.classList.add("main-menu");
-        }
-        
-    });
+window.addEventListener("load",()=>{
+  document.querySelector(".menu-btn").addEventListener("click",()=> {
+    document.querySelector(".nav-menu").classList.toggle("show")
+  })  
+});
