0d200c6ae90f99521d775de87654342587699b81
LobitoM365 <almariojuancho@gmail.com>
2023-10-11 16:33:50 -0500

    arepa

--- js/carrito.js
+++ js/carrito.js
@@ -2,11 +2,17 @@ let boxMinus = document.getElementById("boxMinus");
 let boxCantidad = document.getElementById("boxCantidad");
 let boxPlus = document.getElementById("boxPlus");
 
+let valorProducto = 20000000;
+let auxValorProducto =new Intl.NumberFormat("de-DE").format(valorProducto)
+document.getElementById("precioUnidad").innerHTML=auxValorProducto
+
 boxPlus.addEventListener("click",function(){
 let auxCant= boxCantidad.innerHTML
 if(auxCant < 10){
     auxCant++;
    boxCantidad.innerHTML = auxCant
+   calcularSubtotal()
+   sip()
 }
 })
 boxMinus.addEventListener("click",function(){
@@ -14,5 +20,16 @@ boxMinus.addEventListener("click",function(){
     if(auxCant > 0){
         auxCant--;
        boxCantidad.innerHTML = auxCant
+       calcularSubtotal()
+       sip()
     }
-    })
+})
+function calcularSubtotal(){
+    let auxCant = valorProducto*Number(boxCantidad.innerHTML);
+    let auxCanTUMiL = Intl.NumberFormat("de-DE").format(auxCant);
+    document.getElementById("subtotal").innerHTML="$" + auxCanTUMiL;
+}
+function sip(){
+    let auxCant = boxCantidad.innerHTML;
+    document.getElementById("numeroProductos").innerHTML=auxCant
+}
