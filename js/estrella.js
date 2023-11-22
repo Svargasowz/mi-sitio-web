3d1498e8e32b51a6d1e01155c9cb819e32170a38
LeomarEs19 <lespanacastillo20@gmail.com>
2023-11-22 13:55:19 -0500

    sd

---
+++ js/estrella.js
@@ -0,0 +1,12 @@
+var cali5 = document.getElementById("cali5");
+var cali4 = document.getElementById("cali4");
+var cali3 = document.getElementById("cali3");
+var cali2 = document.getElementById("cali2");
+var cali1 = document.getElementById("cali1");
+var enviar= document.getElementById("enviar");
+var si = document.getElementById("si")
+
+enviar.addEventListener("click",function(){
+    
+    window.alert("el resultado es")
+})
